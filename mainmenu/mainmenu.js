7778e96d2bf24595ddeff4902130da3f3f0fa086
Lou Kielhorn <loukielhorn@gmail.com>
2024-01-06 16:05:40 +0100

    Added splitscreen for a second user. Added a timer. Fixed resizing problems

--- mainmenu/mainmenu.js
+++ mainmenu/mainmenu.js
@@ -1,16 +1,75 @@
 import * as data from "./data.js";
 
+let currentShownMeals = data.foodOnMonday;
 let currentDayMeals = data.foodOnMonday;
-let language;
+let preferredMealsList = [];
+
+let language = "german";
+let secondUser = true;
+
+function startSecondUser(seconds) {
+    toggleSecondUser(true);
+    const timerInterval = setInterval(function() {
+        let timerText = language === 'german' ? ' Sekunden verbleibend' : ' seconds remaining';
+        document.getElementById('timer').textContent = seconds + timerText;
+        seconds--;
+        if (seconds < 0) {
+            clearInterval(timerInterval);
+            toggleSecondUser(false);
+        }
+    }, 1000);
+}
+
+function toggleSecondUser(status) {
+    secondUser = status;
+    const secondUserElement = document.getElementById('secondUser');
+    const separatorElement = document.querySelector('.separator');
+    const headElement = document.querySelector('.head');
+    const timeElement = document.getElementById('timer');
+    timeElement.style.display = status ? "Flex" : "None";
+    secondUserElement.hidden = !status;
+    separatorElement.hidden = !status;
+
+
+    console.log('Second User Element:', secondUserElement);
+    console.log('Separator Element:', separatorElement);
+    console.log('Time Element:', timeElement);
+
+    headElement.style.maxWidth = status ? "50%" : "100%";
+    onWindowResize();
+}
 
 function getLocalStorageData() {
     language = localStorage.getItem('language');
     document.getElementById("language").value = language;
+
+    //preferredMealsList = JSON.parse(localStorage.getItem("preferredMeals"));
 }
 
+window.addEventListener('resize', onWindowResize);
+function onWindowResize() {
+    const contentElement = document.querySelector('.content');
+    const headElement = document.querySelector('.head');
+
+    const weekElement = document.querySelector('.week');
+    const weekOverviewElement = document.getElementById('weekOverview');
+    headElement.style.minWidth = `${weekElement.offsetWidth + weekOverviewElement.offsetWidth + 35}px`;
+
+    const headHeight = headElement.offsetHeight;
+    contentElement.style.marginTop = `${headHeight - 20}px`;
+
+    const completeRightElement = document.querySelector('.completeRight');
+    if(headHeight > 100) {
+        completeRightElement.style.marginLeft = "0px";
+    } else {
+        completeRightElement.style.marginLeft = "auto";
+    }
+}
 
 window.onload = function() {
     //Standart Einstellungen
+    startSecondUser(60);
+    //toggleSecondUser(false);
     getLocalStorageData();
     updateDishes();
     handleLanguageChange();
@@ -32,12 +91,36 @@ window.onload = function() {
 // Funktion zum Aktualisieren der Gerichte
 function updateDishes() {
     let otherMeals = document.getElementById("otherMeals");
-    let sortBy = document.getElementById("sorting").selectedIndex;
+    let preferredMeals = document.getElementById("preferredMeals");
+    let mealsToday = document.getElementById("mealsToday");
+    let title_preferredMeals = document.getElementById("title_preferredMeals");
+
     otherMeals.innerHTML = '';
+    mealsToday.innerHTML = '';
+    preferredMeals.innerHTML = ''
+
+    let sortBy = document.getElementById("sorting").selectedIndex;
 
     //Sortieren
+    let currentShownMealsSorted = sortMeals(currentShownMeals, sortBy);
+    let preferredMealsSorted = sortMeals(preferredMealsList, sortBy);
+
+    let mealsOfTodayAndPreferred = currentShownMealsSorted.filter(value => preferredMealsSorted.includes(value));
+    title_preferredMeals.hidden = mealsOfTodayAndPreferred.length === 0;
+
+    //Entferne Gerichte die schon bei den lielings Gerichten drinne sind
+    currentShownMealsSorted = currentShownMealsSorted.filter(item => !mealsOfTodayAndPreferred.includes(item));
+
+    insertMealsToMenu(mealsOfTodayAndPreferred, preferredMeals);
+    insertMealsToMenu(currentShownMealsSorted, otherMeals);
+    if(secondUser) {
+        insertMealsToMenu(currentDayMeals, mealsToday);
+    }
+}
+
+function sortMeals(meals, sortBy) {
     let sortOrder = [];
-    for (let id of currentDayMeals) {
+    for (let id of meals) {
         if(sortOrder.length === 0) {
             sortOrder.push(id);
         } else {
@@ -66,33 +149,35 @@ function updateDishes() {
                 sortOrder.push(id);
             }
         }
-
     }
+    return sortOrder;
+}
 
-    for (let id of sortOrder) {
+function insertMealsToMenu(meals, menu) {
+    for (let id of meals) {
         let foodItem = data.foodItems[id];
         let description = language === "german" ? foodItem.description_de : foodItem.description_en;
         let dishHtml = `
-            <div id="dish_${id}" class="dish">
+            <div id="${menu.id}_dish_${id}" class="dish">
                 <div class="dish-head">
                     <img src="${foodItem.image}" alt="${foodItem.name_en}">
                     <div class="price" id="dish_price_${id}">${foodItem.price.toFixed(2).replace('.',',')}€</div>
                 </div>
             </div>
         `;
-        otherMeals.innerHTML += dishHtml;
+        menu.innerHTML += dishHtml;
         let dishDescriptionHtml = `
             <div class="dish-description">
                 ${description}
             </div>
         `;
         if(language !== "only-pictures") {
-            let dish = document.getElementById("dish_" + id);
+            let dish = document.getElementById(menu.id + "_dish_" + id);
             dish.innerHTML += dishDescriptionHtml;
         }
-
     }
 }
+
 
 function handleLanguageChange() {
     language = document.getElementById("language").value;
@@ -111,6 +196,7 @@ function handleLanguageChange() {
         document.getElementById("option_popularity").innerText = "Beliebtheit";
         document.getElementById("title_otherMeals").innerText = "Weitere Gerichte";
         document.getElementById("title_preferredMeals").innerText = "Vorschläge für dich";
+        document.getElementById("title_mealsToday").innerText = "Heutige Gerichte";
     } else {
         document.getElementById("btn_monday").innerText = "Mon";
         document.getElementById("btn_tuesday").innerText = "Tue";
@@ -125,8 +211,10 @@ function handleLanguageChange() {
         document.getElementById("option_popularity").innerText = "Popularity";
         document.getElementById("title_otherMeals").innerText = "Other meals";
         document.getElementById("title_preferredMeals").innerText = "Suggestions for you";
+        document.getElementById("title_mealsToday").innerText = "Today's dishes";
     }
     updateDishes();
+    onWindowResize();
 }
 
 // Funktion zum Hinzufügen von Event-Listenern für die Week Buttons
@@ -134,7 +222,7 @@ function addWeekButtonClickListener(buttonId, foodData) {
     const button = document.getElementById(buttonId);
 
     button.addEventListener('click', function() {
-        currentDayMeals = foodData;
+        currentShownMeals = foodData;
         updateDishes();
 
         // Alle Buttons zurücksetzen
